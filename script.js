fafa5958162edd0393551e470aee8eecf5a66c47
mian Shargeel <call2poma@gmail.com>
2025-01-28 09:26:57 +0100

    Template optimasiert

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
+let recipeContentRef = document.getElementById('recipe-content');
+let basketContentRef = document.getElementById('basketContent');
+let basket = [];
+let subTotal = 0;
+let deliveryFee = 5.00;
+let total = 0;
+
+recipeContentRef.innerHTML = '';
+basketContentRef.innerHTML = '';
+
 function init() {
   emptyBasket();
   renderRecipeTemplate();
+}
+
+function renderRecipeTemplate() {
+  myDishes.forEach((ele, index) => {
+    recipeContentRef.innerHTML += renderRecipeTemplateHtml(ele, index)
+  });
+}
+
+function renderBasket() {
+  let basketContentHtml = '';
+  
+  basket.forEach((item, index) => {
+    basketContentHtml += renderBasketHtml(item, index);
+  });  
+  basketContentRef.innerHTML = basketContentHtml + priceCalculation(); 
+}
+
+function priceCalculation() {
+  for (let i = 0; i < basket.length; i++) {
+    subTotal += (basket[i].price * basket[i].amount)
+  }
+  total = (parseFloat(subTotal + deliveryFee).toFixed(2));
+  return priceCalculationHtml();
 }
 
 function showSmallBasket() {
@@ -54,7 +87,10 @@ function decreaseItemsInBasket(index) {
 }
 
 function placeOrder() {
-  basket.length = 0; 
+  if (basket.length = 0) {
+    total = 0;
+    subTotal = 0;
+  }
   emptyBasket(); 
   hideShowedMessage();
 }
@@ -62,7 +98,7 @@ function placeOrder() {
 function showMessageAfterPlacingOrder() {
   let showMessageRef = document.getElementById('show-message');
 
-  showMessageRef.innerHTML = `Congratulation, Your Order is Placed Successfully`;
+  showMessageRef.innerHTML = `<p>Cuper!, Ihre Bestellung wurde erfolgreich aufgegeben</p>`;
   showMessageRef.style.display = 'block';
 }
 
--- scripts/template.js
+++ scripts/template.js
@@ -1,54 +1,33 @@
-let recipeContentRef = document.getElementById('recipe-content');
-let basketContentRef = document.getElementById('basketContent');
-let basket = [];
-
-recipeContentRef.innerHTML = '';
-basketContentRef.innerHTML = '';
-
-function renderRecipeTemplate() {
-  myDishes.forEach((ele, index) => {
-    recipeContentRef.innerHTML += `
+function renderRecipeTemplateHtml(ele, index) {
+  return `
       <div class="card card-hover mb-2">
         <div class="card-body">
           <div class='choose-recipe'>
           <h4><bold>${ele.name}</bold></h4> <img id='addBtn' onclick='addToBasket(${index})' src="./assets/img/plus.png" alt="" /></div>
           <p>${ele.description}</p>
-          <b class='pizza-price'>${ele.price}€</b>
+          <b class='pizza-price'>${(ele.price.toFixed(2)).replace('.', ',')}€</b>
         </div>
       </div>
     `;
-  });
 }
 
-function renderBasket() {
-  let basketContentHtml = '';
-  
-  basket.forEach((item, index) => {
-    basketContentHtml += `<div className="basketItem"> <p><strong>${item.name}</strong></p>
+function renderBasketHtml(item, index) {
+  return `<div className="basketItem"> <p><strong>${item.name}</strong></p>
         <div class='subItems'>
           <img class='plusMinus' onclick='decreaseItemsInBasket(${index})' src="assets/img/minus.png" alt="" />${item.amount}x  
           <img class='plusMinus' onclick='increaseItemsInBasket(${index})' src="assets/img/plus.png" alt="" />${(item.price.toFixed(2)).replace('.', ',')}€
           <img onclick='delteItemFromBasket(${index})' class='removeBtn' src="./assets/img/delete.png" alt="" /></div>
       </div>
     `;
-  });  
-  basketContentRef.innerHTML = basketContentHtml + priceCalculation(); 
 }
 
-function priceCalculation() {
-  let subTotal = 0;
-  let deliveryFee = 5.00;
-
-  for (let i = 0; i < basket.length; i++) {
-    subTotal += (basket[i].price * basket[i].amount)
-  }
-  let total = (parseFloat(subTotal + deliveryFee).toFixed(2));
+function priceCalculationHtml() {
   return `<div class='sumDetails'> <p><span>Zwischenzumme: ${(subTotal.toFixed(2)).replace('.', ',')}€</span></p>
       <p><span>Lieferkosten: ${(deliveryFee.toFixed(2)).replace('.', ',')}€</span></p> <p><span><strong>Gesamt:</strong> ${total.replace('.', ',')}€</span></p></div>
       <div><button class='btn btn-primary place-order-btn' onclick="placeOrder()">Bestellen</button>
       </div>
   `;
-}
+} 
 
 function emptyBasket() {
   basketContentRef.innerHTML = `
